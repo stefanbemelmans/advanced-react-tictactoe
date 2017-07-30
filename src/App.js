a27a1f19e87ec34d434c8ae962fdc103afd23ed9
stackbaxter <stackbaxter@gmail.com>
2017-07-30 16:16:55 -0500

    board class cometh

--- src/App.js
+++ src/App.js
@@ -6,69 +6,74 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: new Array(9),
+      // board: new Array(9),
       playerTurn: 'X'      
     
     };
     this.baseState = this.state;
     this.reset = this.reset.bind(this);
     this.winSideways = this.winSideways.bind(this);
+    this.renderSquare = this.renderSquare.bind(this);
     }
 
     
 
-    renderSquare(i){
+    // renderSquare(i){
       
-      return <Square onClick={() => this.makeMove(i)} value={this.state.board[i]} />
+    //   return <Square onClick={() => this.makeMove(i)} value={this.state.board[i]} />
 
-      }
-
-    reset(){
-      // let newArr = Array(3).fill(null);
-      console.log("resetme!");
-      this.setState(this.baseState)
+    //   }
+    
+    // renderBoard(grid){
+    //   <Board squares={grid} />
+    // }
+    // reset(){
+    //   // let newArr = Array(3).fill(null);
+    //   console.log("resetme!");
+    //   this.setState(this.baseState)
 
         // board : newArr,
         // playerTurn : 'X'
         
     }
      
-    winSideways(player) {
-      let board = this.state.board;
-    //  let turn = this.state.playerTurn;
-      for(let i =0; i<3; i++){
-        if(board[i] === player && board[i+3] === player && board[i+6] === player){
-            return true;
-        }
-      }
-    }
+    // winSideways(player) {
+    //   let board = this.state.board;
+    // //  let turn = this.state.playerTurn;
+    //   for(let i =0; i<3; i++){
+    //     if(board[i] === player && board[i+3] === player && board[i+6] === player){
+    //         return true;
+    //     }
+    //   }
+    // }
 
-    announceWin(player){
-      console.log('hello')
-      document.getElementById('announce-winner').innerHTML(player + 'Wins!')
-    }
+    // announceWin(player){
+    //   console.log('hello')
+    //   document.getElementById('announce-winner').innerHTML(player + 'Wins!')
+    // }
 
-    makeMove(i) {
-      console.log(i)
-      let temp = this.state.playerTurn;
-      const boardCpy = this.state.board.slice();
-      boardCpy[i] = temp;
+    // makeMove(i) {
+    //   console.log(i)
+    //   let temp = this.state.playerTurn;
+    //   const boardCpy = this.state.board.slice();
+    //   boardCpy[i] = temp;
      
-      this.setState({
-        board: boardCpy,
-      })
-      document.getElementById('board')
-      if(this.winSideways(temp)){
-        this.announceWin(temp);
-      }
-      else{
-        temp === 'X' ? temp = 'O': temp ='X';
-        this.setState({
-          playerTurn: temp
-        })
-      }
+    //   this.setState({
+    //     board: boardCpy,
+    //   })
+    //  document.getElementById(board).innerHTML(<Board board={boardCpy} /> )
+    //   document.getElementById('board')
+    //   if(this.winSideways(temp)){
+    //     this.announceWin(temp);
+    //   }
+    //   else{
+    //     temp === 'X' ? temp = 'O': temp ='X';
+    //     this.setState({
+    //       playerTurn: temp
+    //     })
+    //   }
     
-    }
+    // }
     // if(!pos){
     //   this.renderMove(this.state.playerTurn);
     //   if(this.state.playerTurn === 'X'){
@@ -91,12 +96,12 @@ class App extends Component {
   render() {
 
     return (
-      <div>
+      <div id="board">
         <Board board={this.state.board} renderSquare={this.renderSquare} />
         <div id="announce-winner"></div>
         <button id="clear" onClick={this.reset}>Clear Board</button>
-        </div>
       </div>
+      
     );
   }
 }
--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,62 @@
 import React from "react";
-import Square from "Square";
+import Square from "./Square";
 
-export default function Board(props){
-  
+export default class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      board: new Array(9)
+     }
+    this.renderSquare = this.renderSquare.bind(this);
+  }
+  renderSquare(i){
+      
+      return <Square onClick={() => this.makeMove(i)} value={this.state.board[i]} />
+
+      }
+
+  makeMove(i) {
+      console.log(i)
+      let temp = this.state.playerTurn;
+      const boardCpy = this.state.board.slice();
+      boardCpy[i] = temp;
+     
+      this.setState({
+        board: boardCpy,
+      })
+     document.getElementById(board).innerHTML(<Board board={boardCpy} /> )
+      document.getElementById('board')
+      if(this.winSideways(temp)){
+        this.announceWin(temp);
+      }
+      else{
+        temp === 'X' ? temp = 'O': temp ='X';
+        this.setState({
+          playerTurn: temp
+        })
+      }
+    
+    }
+  winSideways(player) {
+      let board = this.state.board;
+    //  let turn = this.state.playerTurn;
+      for(let i =0; i<3; i++){
+        if(board[i] === player && board[i+3] === player && board[i+6] === player){
+            return true;
+        }
+      }
+    }
+
+  announceWin(player){
+    console.log('hello')
+    document.getElementById('announce-winner').innerHTML(player + 'Wins!')
+  }
+
+  render(){
+
+
   return(
-    <div id="board">
+    <div>
       <div className="row">
         {props.renderSquare(0)}
         {props.renderSquare(1)}
@@ -24,6 +76,6 @@ export default function Board(props){
   );
 
 }
-
-
 }
+
+
